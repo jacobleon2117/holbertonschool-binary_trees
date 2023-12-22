8fb48cde53d01f8231ca66ae0f490e7cf6b55e46
jacobleon2117 <jacobleon2117@gmail.com>
2023-12-22 03:08:20 -0600

    adding file

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - delete a binary tree from memory
+ * @tree: the tree being deleted
+*/
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree != NULL)
+	{
+		if (tree->left != NULL)
+			binary_tree_delete(tree->left);
+		if (tree->right != NULL)
+			binary_tree_delete(tree->right);
+		free(tree);
+	}
+}
