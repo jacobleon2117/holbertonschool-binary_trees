67957202e246d93f2daba8ef55b5ade06a4255bc
jacobleon2117 <jacobleon2117@gmail.com>
2023-12-22 02:42:52 -0600

    file/code/whitespace

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - makes a binary tree node
+ * @parent: pointer to parent node of new node
+ * @value: value associated with new node
+ * Return: pointer to new node, or NULL
+ **/
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newNode = NULL;
+
+	newNode = malloc(sizeof(binary_tree_t));
+	if (newNode == NULL)
+	{
+		return (NULL;)
+	}
+	
+	newNode->n = value;
+	newNode->parent = parent;
+	newNode->right = NULL;
+	newNode->left = NULL;
+
+	return (newNode);
+}
