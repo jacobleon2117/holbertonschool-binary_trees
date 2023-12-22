61e504c2df560417c3a588623fa0d84e34b784a7
jacobleon2117 <143765559+jacobleon2117@users.noreply.github.com>
2023-12-22 03:34:20 -0600

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -2,10 +2,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - determines the height of a binary tree
- * @tree: the tree being measured
+ * binary_tree_height - height of a binary tree
+ * @tree: tree being measured
  *
- * Return: the height of the tree. Defaults to 0 if tree is NULL
+ * Return: height of the tree. Defaults to 0 if tree is NULL
 */
 
 size_t binary_tree_height(const binary_tree_t *tree)
